c4825bb0917e0f020b660ad25740223908b5cb48
Admin <admin@admins-mbp.payoda.com>
2016-02-13 10:24:12 +0530

    Day03

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 var http = require("http");
+var fs = require("fs");
+
 var server = http.createServer(function(request,response){
-	response.end('Hello World');
+	var url = request.url;
+	var filePath = url.substring(1);
+	loadFile(filePath,response);
 });
+var loadFile = function(filePath,response){
+	fs.readFile(filePath,function(e,data){
+		if(e)
+			response.end("Error. File not found " + filePath);
+		else
+			response.end(data);
+	});
+};
 server.listen(8080);
 console.log("Server started and running in 8080");
